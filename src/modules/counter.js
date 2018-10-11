e8b458a4f5d398e8781db6f4437bbfcc5f8cc9d6
garenjungle <kykps2@gmail.com>
2018-10-12 02:15:38 +0900

    second push

---
+++ src/modules/counter.js
@@ -0,0 +1,27 @@
+import { handleActions, createAction } from 'redux-actions';
+
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+
+export const increment = createAction(INCREMENT);
+export const decrement = createAction(DECREMENT);
+
+export const incrementAsync = () => dispatch => {
+  setTimeout(() => {
+    dispatch(increment());
+  }, 1000);
+};
+
+export const decrementAsync = () => dispathch => {
+  setTimeout(() => {
+    dispathch(decrement());
+  }, 1000);
+};
+
+export default handleActions(
+  {
+    [INCREMENT]: (state, action) => state + 1,
+    [DECREMENT]: (state, action) => state - 1
+  },
+  1
+);
